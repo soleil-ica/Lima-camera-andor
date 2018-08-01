927038ea975ce25a47e3421e1cec450232aa9123
Samuel Debionne <samuel.debionne@esrf.fr>
2018-08-01 17:15:19 +0200

    Update the build sytem to support devel conda package

---
+++ cmake/FindAndor.cmake
@@ -0,0 +1,17 @@
+set(ANDOR_INCLUDE_DIRS)
+set(ANDOR_LIBRARIES)
+set(ANDOR_DEFINITIONS)
+
+if(WIN32)
+  # Missing find_path here
+  find_library(ANDOR_LIBRARIES atmcd64m)
+else()
+  find_path(ANDOR_INCLUDE_DIRS "atmcdLXd.h")
+  find_library(ANDOR_LIBRARIES andor)
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Andor DEFAULT_MSG
+  ANDOR_LIBRARIES
+  ANDOR_INCLUDE_DIRS
+)
